43f3271d2904ebdbae492373c2a77c3f976b36f5
nsaravanas <saravanagceb@gmail.com>
2016-11-10 22:49:39 +0530

    initial commit

---
+++ elasticsearch/src/main/java/com/example/ElasticSearchApp.java
@@ -0,0 +1,35 @@
+package com.example;
+
+import java.io.IOException;
+import java.net.InetAddress;
+
+import org.elasticsearch.action.get.GetResponse;
+import org.elasticsearch.action.index.IndexResponse;
+import org.elasticsearch.client.transport.TransportClient;
+import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.common.transport.InetSocketTransportAddress;
+import org.elasticsearch.common.xcontent.XContentBuilder;
+import org.elasticsearch.common.xcontent.XContentFactory;
+
+public class ElasticSearchApp {
+
+	public static void main(String[] args) throws IOException {
+
+		Settings settings = Settings.settingsBuilder().build();
+		TransportClient client = TransportClient.builder().settings(settings).build()
+				.addTransportAddress(new InetSocketTransportAddress(InetAddress.getByName("localhost"), 9300));
+		System.out.println(client.connectedNodes());
+
+		XContentBuilder builder = XContentFactory.jsonBuilder().startObject().field("name", "Saravana").field("age", 27).field("country", "India")
+				.field("citizen", "Indian").endObject();
+
+		IndexResponse indexResponse = client.prepareIndex("twitter", "tweet", "1").setSource(builder).get();
+		System.out.println(indexResponse);
+
+		GetResponse getResponse = client.prepareGet("twitter", "tweet", "1").setOperationThreaded(false).get();
+		System.out.println(getResponse.getSource());
+
+		client.close();
+	}
+
+}
---
+++ elasticsearch/src/main/java/com/example/ExcelToES.java
@@ -0,0 +1,136 @@
+package com.example;
+
+import java.io.FileInputStream;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.DateUtil;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.elasticsearch.client.transport.TransportClient;
+import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.common.transport.InetSocketTransportAddress;
+
+public class ExcelToES {
+
+	private TransportClient client;
+
+	public void init() {
+		Settings settings = Settings.settingsBuilder().build();
+		try {
+			this.client = TransportClient.builder().settings(settings).build()
+					.addTransportAddress(new InetSocketTransportAddress(InetAddress.getByName("localhost"), 9300));
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void main(String[] args) {
+		String index = null;
+		String file1 = null;
+		String source1 = null;
+		String file2 = null;
+		String source2 = null;
+		if (args.length != 5 && args.length != 3) {
+			System.out.println("Usage: indexName odhFile dmFile");
+			System.out.println("or");
+			System.out.println("Usage: indexName odhFile odhKey dmFile dmKey");
+			System.exit(0);
+		}
+		if (args.length == 3) {
+			index = args[0];
+			file1 = args[1];
+			source1 = "odh";
+			file2 = args[2];
+			source2 = "dm";
+		}
+		if (args.length == 5) {
+			index = args[0];
+			file1 = args[1];
+			source1 = args[2];
+			file2 = args[3];
+			source2 = args[4];
+		}
+		ExcelToES ejson = new ExcelToES();
+		ejson.init();
+		boolean success1 = ejson.processExcel(index, source1, file1);
+		boolean success2 = ejson.processExcel(index, source2, file2);
+		if (!success1 || !success2) {
+			System.err.println("some error occurred");
+		}
+		ejson.close();
+	}
+
+	private boolean processExcel(String index, String source, String filePath) {
+		int counter = 0;
+		try {
+			FileInputStream fis = new FileInputStream(filePath);
+			Workbook workbook = new HSSFWorkbook(fis);
+			Sheet sheet = workbook.getSheetAt(0);
+			List<String> header = new ArrayList<String>();
+			Row headerRow = sheet.getRow(0);
+			for (Cell cell : headerRow) {
+				header.add(cell.getRichStringCellValue().getString().toLowerCase());
+			}
+			boolean ignore = false;
+			for (Row row : sheet) {
+				if (!ignore) {
+					ignore = true;
+					continue;
+				}
+				StringBuilder sb = new StringBuilder();
+				sb.append("{ ");
+				for (int i = 0; i < header.size(); i++) {
+					Cell cell = row.getCell(i);
+					Object obj = getValue(cell);
+					sb.append("\"").append(header.get(i)).append("\" : \"").append(obj).append("\" , ");
+				}
+				sb.append("\"source\" : \"" + source + "\" } ");
+				this.indexDocument(index, source, ++counter + "", sb.toString());
+				sb.setLength(0);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			return false;
+		}
+		return true;
+	}
+
+	private static Object getValue(Cell cell) {
+		Object value = null;
+		switch (cell.getCellType()) {
+		case Cell.CELL_TYPE_STRING:
+			value = cell.getRichStringCellValue();
+			break;
+		case Cell.CELL_TYPE_NUMERIC:
+			if (DateUtil.isCellDateFormatted(cell)) {
+				Date date = cell.getDateCellValue();
+				value = new SimpleDateFormat("yyyy-MM-dd").format(date);
+			} else {
+				value = cell.getNumericCellValue();
+			}
+			break;
+		case Cell.CELL_TYPE_BOOLEAN:
+			value = cell.getBooleanCellValue();
+			break;
+		default:
+			value = "";
+		}
+		return value;
+	}
+
+	public void close() {
+		client.close();
+	}
+
+	private void indexDocument(String index, String type, String id, String source) {
+		client.prepareIndex(index, type, id).setSource(source).get().getIndex();
+	}
+}
